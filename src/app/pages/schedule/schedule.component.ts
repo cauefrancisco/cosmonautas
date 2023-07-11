468cd276693e99725b4121bed185821475ebca73
cauefrancisco <cauefranscis@gmail.com>
2023-07-11 15:40:23 -0300

    back buttom

--- src/app/pages/schedule/schedule.component.ts
+++ src/app/pages/schedule/schedule.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-schedule',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScheduleComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _router: Router,
+  ) { }
 
   ngOnInit() {
+  }
+
+  public goTo(page: string): void {
+    this._router.navigateByUrl(page);
   }
 
 }
